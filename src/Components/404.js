731ec940e8fc1f6dc28a26a68b09c836d5ca12f6
Chris Aragon <aragonchristopherray@gmail.com>
2020-08-20 17:50:16 -0400

    first commit

---
+++ src/Components/404.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+export default class FourOFour extends Component{
+    render(){
+        return(
+            <p>Your input number could not be found at this time :(</p>
+        )
+    }
+}
