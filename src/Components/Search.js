aca59fc32edcfcb0e9f3fdba1490d7d0cc4c74d7
Chris Aragon <aragonchristopherray@gmail.com>
2020-08-21 11:40:42 -0400

    i need help :(

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -4,15 +4,24 @@ import FourOFour from './404';
 export default class Search extends Component{
     constructor(props){
         super(props);
-        this.linearSearch = this.linearSearch.bind(this)
+        this.state = {
+            value: ''
+        }
     }
-    linearSearch = (value, e) => {
+    handleChange = (e) => {
+        console.log('the handle click has activated')
+        this.setState({
+            value: e.target.value
+        })
+    }
+    linearSearch = (e, value) =>{
         e.preventDefault();
         console.log('this was clicked')
+        console.log(this.state.value)
         const array = [89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62, 93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5]
         for (let i = 0; i < array.length; i++){
-            if(value === array[i].value){
-                return value
+            if(this.state.value === array[i].value){
+                console.log("the loop ran!")
             }
         }
         return FourOFour;
@@ -21,9 +30,9 @@ export default class Search extends Component{
         return(
             <>
                 <div>
-                    <form onClick={e => this.linearSearch}>
+                    <form onSubmit = {this.linearSearch}>
                         <label for="value">Value</label>
-                        <input type="number" id = "search" name="value" required/>
+                        <input type="number" id = "search" value={this.state.value} onChange = {this.handleChange} required/>
                         <button>Submit!</button>
                     </form>
                 </div>
