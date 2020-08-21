8a865ce6aadfc7788d2dbaad1cf971a208fd8d39
Chris Aragon <aragonchristopherray@gmail.com>
2020-08-21 18:02:44 -0400

    why can't this be easy

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -35,27 +35,31 @@ export default class Search extends Component{
         }
         return FourOFour;
     }
-    binarySearch = (e) => {
+    binarySearch = (e, binaryValue, start, end) => {
         e.preventDefault();
         const array = [89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62, 93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5]
-        let start = start === undefined ? 0 : start;
-        let end = end === undefined ? array.length : end;
+        var start = start === undefined ? 0 : start;
+        var end = end === undefined ? array.length : end;
+        let searches = 0
         console.log(start, end)
         if (start > end){
             return -1
         }
 
-        const index = Math.floot((start + end) / 2);
+        const index = Math.floor((start + end) / 2);
         const item = array[index];
 
         if (item == this.state.binaryValue){
+            console.log(searches)
             return index;
         }
         else if (item < this.state.binaryValue){
-            return this.binarySearch(e)
+            searches++
+            return this.binarySearch(e, binaryValue, index + 1, end)
         }
         else if (item > this.state.binaryValue){
-            return this.binarySearch(e)
+            searches++
+            return this.binarySearch(e, binaryValue, start, index - 1)
         }
         
     }
