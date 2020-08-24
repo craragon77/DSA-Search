ece5f6bfa820e0bf2ee6230c4d716c1b8d725459
Chris Aragon <aragonchristopherray@gmail.com>
2020-08-24 12:28:14 -0400

    yee

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -35,7 +35,7 @@ export default class Search extends Component{
         }
         return FourOFour;
     }
-    binarySearch = (e, binaryValue, start, end) => {
+    binarySearch = (e, start, end) => {
         e.preventDefault();
         const array = [89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62, 93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5]
         var start = start === undefined ? 0 : start;
@@ -55,11 +55,11 @@ export default class Search extends Component{
         }
         else if (item < this.state.binaryValue){
             searches++
-            return this.binarySearch(e, binaryValue, index + 1, end)
+            return this.binarySearch(e, this.state.binaryValue, index + 1, end)
         }
         else if (item > this.state.binaryValue){
             searches++
-            return this.binarySearch(e, binaryValue, start, index - 1)
+            return this.binarySearch(e, this.state.binaryValue, start, index - 1)
         }
         
     }
